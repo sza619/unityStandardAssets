957d0c94fb42cd0ee068421a99eec328ef9ccbec
David Åse <davidase@telenordigital.com>
2015-05-06 18:45:50 +0200

    finish jump logic

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,44 +3,67 @@ using System.Collections;
 
 public class Move : MonoBehaviour {
 
-	private float xStartLimit = 0;
-	private float xStopLimit = 0;
+	private float xStartPos = 0;
+	private float xStopPos = 0;
+	private int currentStep = 0;
 	public float jumpHeight = 2.5f;
 	public float movementSpeed = 1.5f;
+	public bool jumping = false;
 
-	public static float platFormWidth = 3.7f;
-	public float funnelOneStart 	  = -6-platFormWidth/2;
-	public float funnelOneStop 		  = -6+platFormWidth/2;
-	public float funnelTwoStart 	  = -2-platFormWidth/2;
-	public float funnelTwoStop 		  = -2+platFormWidth/2;
-	public float funnelThreeStart 	  =  2-platFormWidth/2;
-	public float funnelThreeStop 	  =  2+platFormWidth/2;
-	public float funnelFourStart 	  =  6-platFormWidth/2;
-	public float funnelFourStop 	  =  6+platFormWidth/2;
+	public bool aboutToJump;
+	public bool aboutToDie;
+
+	public float stepOffset    = 1.85f;
+	public int[] stepPos       = {-6, -2, 2, 6};
+	public float stepOnePos    = -6;
+	public float stepTwoPos    = -2;
+	public float stepThreePos  =  2;
+	public float stepFourPos   =  6;
 
 	void Start () {
-		print (GetComponent<Rigidbody> ().position);
-		GetComponent<Rigidbody> ().position = new Vector3 (funnelOneStart+1, 0, 0);	
-		GetComponent<Rigidbody> ().velocity = new Vector3(movementSpeed, 0, 0);	
+		xStartPos = stepPos[currentStep]-stepOffset;
+		xStopPos = stepPos[currentStep]+stepOffset;
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.position = new Vector3 (xStartPos+1, 0, 0);	
+		rigidbody.velocity = new Vector3(movementSpeed, 0, 0);	
 	}
 
 	void FixedUpdate () {
-		xStartLimit = funnelOneStart;
-		xStopLimit = funnelOneStop;
-		Rigidbody rigidBody = GetComponent<Rigidbody>();
-		float xPos = rigidBody.position.x;
-		if (Input.GetKeyDown(KeyCode.W)) {
-			rigidBody.angularVelocity = Vector3.zero;
-			rigidBody.velocity = new Vector3(movementSpeed, 0, 0);
-			rigidBody.AddForce(Vector3.up*jumpHeight, ForceMode.VelocityChange);
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		float xPos = rigidbody.position.x;
+		if (Input.GetKeyDown(KeyCode.Y)) {
+			aboutToJump = true;
 		}
-		if (xPos <= this.funnelOneStop && xPos >= this.funnelOneStart) {
-			rigidBody.velocity = new Vector3(movementSpeed, rigidBody.velocity.y, 0);
+		if (xPos <= xStopPos && xPos >= xStartPos) {
+			rigidbody.velocity = new Vector3(movementSpeed, rigidbody.velocity.y, 0);
 		}
-		if (xPos >= this.funnelOneStop || xPos <= this.funnelOneStart) {
-			movementSpeed = movementSpeed*-1;
-			rigidBody.velocity = new Vector3(movementSpeed, rigidBody.velocity.y, 0);
-			transform.Rotate(new Vector3(0, 180, 0));
+		if (xPos >= xStopPos || xPos <= xStartPos) {
+			if(aboutToJump && xPos >= xStopPos && xPos < stepFourPos) {
+				jump(rigidbody);
+			} else {
+				turnAround(rigidbody);
+			}
 		}
+	}
+
+	void jump(Rigidbody rigidbody) {
+		rigidbody.angularVelocity = Vector3.zero;
+		rigidbody.velocity = new Vector3(movementSpeed, 0, 0);
+		rigidbody.AddForce(Vector3.up*jumpHeight, ForceMode.VelocityChange);
+		aboutToJump = false;
+		currentStep++;
+		StartCoroutine(updatePositions());
+	}
+
+	void turnAround(Rigidbody rigidbody) {
+		movementSpeed = movementSpeed*-1;
+		rigidbody.velocity = new Vector3(movementSpeed, rigidbody.velocity.y, 0);
+		transform.Rotate(new Vector3(0, 180, 0));
+	}
+
+	IEnumerator updatePositions() {
+		xStopPos = stepPos[currentStep]+stepOffset;
+		yield return new WaitForSeconds(1);
+		xStartPos = stepPos[currentStep]-stepOffset;
 	}
 }
