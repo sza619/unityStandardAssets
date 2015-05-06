a77f9a56725395d0f26b892bbb32c0cadd98f411
Christian Jonassen <cjonassen@telenordigital.com>
2015-05-06 22:37:25 +0200

    Remove obsolete stuff that does not work

--- Assets/SpawnLemmings.cs
+++ Assets/SpawnLemmings.cs
@@ -8,25 +8,15 @@ public class SpawnLemmings : MonoBehaviour {
 	List<Transform> lemmingList = new List<Transform>();
 
 	void Start () {
-		 
 		for (var i=0; i<5; i++) {
 			Transform lemming = Instantiate (spawnLemmings) as Transform;
 			lemming.GetComponent<Move>().movementSpeed+=i;
 			lemmingList.Add(lemming);
 		}
 
-		for (var i=0; i<5; i++) {
-			for (var j=0; j<i; j++) {
-				Physics.IgnoreCollision (lemmingList[i].GetComponent<BoxCollider> (), lemmingList[j].GetComponent<BoxCollider> ());
-			}
-		}
 	}
 	
 	void Update () {
-		for (var i=0; i<5; i++) {
-			for (var j=0; j<i; j++) {
-				Physics.IgnoreCollision (lemmingList[i].GetComponent<BoxCollider> (), lemmingList[j].GetComponent<BoxCollider> ());
-			}
-		}
 	}
+
 }
