ca151c23cbb0f2adaa815bf71296c54c7f0653e7
Christian Jonassen <cjonassen@telenordigital.com>
2015-05-06 22:36:16 +0200

    BOOM

---
+++ Assets/SpawnLemmings.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnLemmings : MonoBehaviour {
+
+	public Transform spawnLemmings;
+	List<Transform> lemmingList = new List<Transform>();
+
+	void Start () {
+		 
+		for (var i=0; i<5; i++) {
+			Transform lemming = Instantiate (spawnLemmings) as Transform;
+			lemming.GetComponent<Move>().movementSpeed+=i;
+			lemmingList.Add(lemming);
+		}
+
+		for (var i=0; i<5; i++) {
+			for (var j=0; j<i; j++) {
+				Physics.IgnoreCollision (lemmingList[i].GetComponent<BoxCollider> (), lemmingList[j].GetComponent<BoxCollider> ());
+			}
+		}
+	}
+	
+	void Update () {
+		for (var i=0; i<5; i++) {
+			for (var j=0; j<i; j++) {
+				Physics.IgnoreCollision (lemmingList[i].GetComponent<BoxCollider> (), lemmingList[j].GetComponent<BoxCollider> ());
+			}
+		}
+	}
+}
