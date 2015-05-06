692d7cac5fdfdbab40c4815791e2519a7cfbb025
Christian Jonassen <cjonassen@telenordigital.com>
2015-05-06 22:56:07 +0200

    Try now

--- Assets/SpawnLemmings.cs
+++ Assets/SpawnLemmings.cs
@@ -8,15 +8,48 @@ public class SpawnLemmings : MonoBehaviour {
 	List<Transform> lemmingList = new List<Transform>();
 
 	void Start () {
-		for (var i=0; i<5; i++) {
-			Transform lemming = Instantiate (spawnLemmings) as Transform;
-			lemming.GetComponent<Move>().movementSpeed+=i;
-			lemmingList.Add(lemming);
-		}
 
 	}
-	
+
+	int frame=0;
 	void Update () {
+		frame++;
+
+		if (frame % 15 != 0) {
+			return;
+		}
+
+		var n = Random.Range(0, 3);
+		switch (n) {
+		case 0:
+			MakeALemmingJump ();
+			break;
+		case 1:
+			MakeALemmingDie ();
+			break;
+		case 2:
+			MakeALemmingSpawn ();
+			break;
+		}
+	}
+
+	void MakeALemmingJump() {
+		var ind = randomLemmingIndex ();
+		lemmingList [ind].GetComponent<Move> ().aboutToJump = true;
+	}
+
+	void MakeALemmingDie() {
+		var ind = randomLemmingIndex ();
+		lemmingList [ind].GetComponent<Move> ().aboutToDie = true;
+	}
+
+	void MakeALemmingSpawn() {
+		Transform lemming = Instantiate (spawnLemmings) as Transform;
+		lemmingList.Add(lemming);
+	}
+
+	int randomLemmingIndex() {
+		return Random.Range(0, lemmingList.Count);
 	}
 
 }
