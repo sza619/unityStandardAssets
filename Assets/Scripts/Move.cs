76dc06e70271ed781fa357a663cb1cdc15f0ae10
David Åse <davidase@telenordigital.com>
2015-05-06 15:59:31 +0200

    refactor

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class Move : MonoBehaviour {
 
-	private bool jumping = false;
 	private float limitx = 0;
 	private float limity = 0;
 	public float jumpHeight = 5f;
+
+	public static float platFormWidth = 3.7f;
+	public float funnelOneStart 	= -6-platFormWidth/2;
+	public float funnelOneStop 		= -6+platFormWidth/2;
+	public float funnelTwoStart 	= -2-platFormWidth/2;
+	public float funnelTwoStop 		= -2+platFormWidth/2;
+	public float funnelThreeStart 	=  2-platFormWidth/2;
+	public float funnelThreeStop 	=  2+platFormWidth/2;
+	public float funnelFourStart 	=  6-platFormWidth/2;
+	public float funnelFourStop 	=  6+platFormWidth/2;
+
 	
 	void Start () {
-		//set position to first column
+		print (GetComponent<Rigidbody> ().position);
+		//GetComponent<Rigidbody> ().position = new Vector3 (funnelOneStart, 0, -5);	
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate () {
 		if (Input.GetKeyDown(KeyCode.W)) {
 			GetComponent<Rigidbody>().velocity = Vector3.zero;
