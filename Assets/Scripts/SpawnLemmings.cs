37111926791298545f46f0de2af83a18e6494334
Christian Jonassen <cjonassen@telenordigital.com>
2015-05-06 23:34:29 +0200

    Some simulator

--- Assets/Scripts/SpawnLemmings.cs
+++ Assets/Scripts/SpawnLemmings.cs
@@ -3,72 +3,72 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SpawnLemmings : MonoBehaviour {
-
+	
 	public Transform spawnLemmings;
 	List<Transform> lemmingList = new List<Transform>();
-
+	
 	void Start () {
-
+		
 	}
-
+	
 	int frame = 0;
 	void Update () {
-
+		
 		frame++;
-
-		if (frame % 15 != 0) {
+		
+		if (frame % 30 != 0) {
 			return;
 		}
-
+		
 		var n = Random.Range(0, 3);
 		switch (n) {
 		case 0:
-			MakeALemmingJump();
+			MakeALemmingJump ();
 			break;
 		case 1:
-			MakeALemmingDie();
+			MakeALemmingDie ();
 			break;
 		case 2:
-			MakeALemmingSpawn();
+			MakeALemmingSpawn ();
 			break;
 		}
-
-
-
+		
 		if (frame > 1000) {
 			for (var i=0; i<lemmingList.Count; i++) {
 				lemmingList[i].GetComponent<Move>().aboutToJump = true;
 			}
 		}
-
+		
 		if (frame > 1500) {
 			MakeALemmingSpawn ();
 		}
-
+		
 		if (frame > 2000) {
 			for (var i=0; i<lemmingList.Count; i++) {
 				lemmingList[i].GetComponent<Move>().movementSpeed++;
 			}
 		}
 	}
-
+	
 	void MakeALemmingJump() {
 		var ind = randomLemmingIndex();
 		lemmingList[ind].GetComponent<Move>().aboutToJump = true;
+		lemmingList.RemoveAt (ind);
 	}
-
+	
 	void MakeALemmingDie() {
 		var ind = randomLemmingIndex();
 		lemmingList[ind].GetComponent<Move>().aboutToDie = true;
+		lemmingList.RemoveAt (ind);
 	}
-
+	
 	void MakeALemmingSpawn() {
 		Transform lemming = Instantiate (spawnLemmings) as Transform;
 		lemmingList.Add(lemming);
 	}
-
+	
 	int randomLemmingIndex() {
 		return Random.Range(0, lemmingList.Count-1);
 	}
-
+	
 }
