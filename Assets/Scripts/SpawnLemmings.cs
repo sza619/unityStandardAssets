2167695a46c97e7b9bc2835d12c8cb01c8510216
Christian Jonassen <cjonassen@telenordigital.com>
2015-05-07 12:44:43 +0200

    Label variables added, no logic for them THO

--- Assets/Scripts/SpawnLemmings.cs
+++ Assets/Scripts/SpawnLemmings.cs
@@ -1,20 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class SpawnLemmings : MonoBehaviour {
-	
+
+	public Transform singupform;
+	public Transform singupformDeath;
+
+	public Transform verifyPin;
+	public Transform verifyPinDeath;
+
+	public Transform confirmationPage;
+	public Transform confirmationPageDeath;
+
+	public Transform redirected;
+	public Transform redirectedCastle;
+
 	public Transform spawnLemmings;
 	List<Transform> lemmingList = new List<Transform>();
 	
 	void Start () {
-		
+		MakeALemmingSpawn ();
 	}
 	
 	int frame = 0;
 	void Update () {
-		
+		CountAndUpdateLabels ();
 		frame++;
+
+		if (frame > 3000) {
+			singupform.GetComponent<TextMesh> ().text = "LOL";
+			singupformDeath.GetComponent<TextMesh> ().text = "LOL";
+			
+			verifyPin.GetComponent<TextMesh> ().text = "LOL";
+			verifyPinDeath.GetComponent<TextMesh> ().text = "LOL";
+			
+			confirmationPage.GetComponent<TextMesh> ().text = "LOL";
+			confirmationPageDeath.GetComponent<TextMesh> ().text = "LOL";
+			
+			redirected.GetComponent<TextMesh> ().text = "LOL";
+			redirectedCastle.GetComponent<TextMesh> ().text = "LOL";
+		}
+
+		if (frame < 5) {
+			MakeALemmingSpawn ();
+		}
 		
 		if (frame % 30 != 0) {
 			return;
@@ -35,7 +66,9 @@ public class SpawnLemmings : MonoBehaviour {
 		
 		if (frame > 1000) {
 			for (var i=0; i<lemmingList.Count; i++) {
-				lemmingList[i].GetComponent<Move>().aboutToJump = true;
+				if (lemmingList[i] != null) {
+					lemmingList[i].GetComponent<Move>().aboutToJump = true;
+				}
 			}
 		}
 		
@@ -45,30 +78,58 @@ public class SpawnLemmings : MonoBehaviour {
 		
 		if (frame > 2000) {
 			for (var i=0; i<lemmingList.Count; i++) {
-				lemmingList[i].GetComponent<Move>().movementSpeed++;
+				if (lemmingList[i] != null) {
+					lemmingList[i].GetComponent<Move>().movementSpeed++;
+					singupform.GetComponent<TextMesh>().text="LOL";
+					singupform.GetComponent<TextMesh>().text="LOL";
+					singupform.GetComponent<TextMesh>().text="LOL";
+					singupform.GetComponent<TextMesh>().text="LOL";
+					singupform.GetComponent<TextMesh>().text="LOL";
+					singupform.GetComponent<TextMesh>().text="LOL";
+					singupform.GetComponent<TextMesh>().text="LOL";
+
+				}
 			}
 		}
+
+
 	}
 	
 	void MakeALemmingJump() {
 		var ind = randomLemmingIndex();
-		lemmingList[ind].GetComponent<Move>().aboutToJump = true;
-		lemmingList.RemoveAt (ind);
+		if (lemmingList [ind] != null) {
+			lemmingList [ind].GetComponent<Move> ().aboutToJump = true;
+			lemmingList.RemoveAt (ind);
+		}
 	}
 	
 	void MakeALemmingDie() {
 		var ind = randomLemmingIndex();
-		lemmingList[ind].GetComponent<Move>().aboutToDie = true;
+		if (lemmingList [ind] != null) {
+			lemmingList [ind].GetComponent<Move> ().aboutToDie = true;
+		}
 		lemmingList.RemoveAt (ind);
 	}
 	
 	void MakeALemmingSpawn() {
-		Transform lemming = Instantiate (spawnLemmings) as Transform;
+		Transform lemming = Instantiate(spawnLemmings, new Vector3(-7.081089f, 0.0f, 0.0f), Quaternion.identity) as Transform;
+
 		lemmingList.Add(lemming);
 	}
 	
 	int randomLemmingIndex() {
-		return Random.Range(0, lemmingList.Count-1);
+		return Random.Range(0, lemmingList.Count);
+	}
+
+	void CountAndUpdateLabels() {
+		for (var i=0; i<lemmingList.Count; i++) {
+			if (lemmingList[i] != null) {
+				switch (lemmingList[i].GetComponent<Move>().currentStep) {
+				case 0:
+					break;
+				}
+			}
+		}
 	}
 	
 }
