25a698c083c23e859547fa5bc9c480749878732a
tipsy <davidaase@hotmail.com>
2015-05-12 18:56:00 +0200

    fix memory leak

---
+++ Assets/Scripts/AvoidMemoryLeak.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AvoidMemoryLeak : MonoBehaviour {
+	private bool started = false;
+	private float timeLeft = 5;
+	
+	public void Update() {
+		ParticleSystem system = GetComponent<ParticleSystem>();
+		if (system.isPlaying && started == false) {
+			started = true;
+		}
+		timeLeft -= Time.deltaTime;
+		if (timeLeft <= 0 && started == true) {
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
